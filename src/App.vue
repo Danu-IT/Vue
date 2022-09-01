07fbc73a09eafc1b27bffb3cc2199aeb69187fc4
Danu-IT <d_tareev@mail.ru>
2022-09-01 15:45:13 +0300

    FiveCommit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,77 @@
 <template>
 	<div id="app">
-		<Calc />
+		<!-- <Calc /> -->
+		<!-- <Counter v-if="show" />
+		<button @click="show = !show">Show / Hide</button> -->
+		<header>
+			<div class="header">My personal costs</div>
+		</header>
+		<main>
+			<button class="" @click="show = !show">ADD NEW COST</button>
+			<AddPaymentForm class="formList" v-if="show" @add-payment="addPayment">
+			</AddPaymentForm>
+			<PaymentsDisplay class="dataList" :paymentsList="List"></PaymentsDisplay>
+		</main>
 	</div>
 </template>
 
 <script>
-	import Calc from "@/components/Calc.vue";
+	// import Calc from "@/components/Calc.vue";
+	// import Counter from "@/components/Counter.vue";
+	import PaymentsDisplay from "@/components/PaymentsDisplay.vue";
+	import AddPaymentForm from "@/components/AddPaymentForm.vue";
 
 	export default {
 		name: "App",
 		components: {
-			Calc,
+			// Calc,
+			// Counter,
+			// eslint-disable-next-line vue/no-unused-components
+			PaymentsDisplay,
+			// eslint-disable-next-line vue/no-unused-components
+			AddPaymentForm,
 		},
-		data: () => ({}),
-		methods: {},
+		data: () => ({
+			show: false,
+			List: [],
+		}),
+		methods: {
+			fetchPaymentsData() {
+				return [
+					{
+						date: "28.03.2020",
+						category: "Food",
+						value: 169,
+					},
+					{
+						date: "24.03.2020",
+						category: "Transport",
+						value: 360,
+					},
+					{
+						date: "24.03.2020",
+						category: "Food",
+						value: 532,
+					},
+				];
+			},
+			addPayment(data) {
+				console.log(data);
+				this.List.push(data);
+			},
+		},
+		created() {
+			setTimeout(() => {
+				this.List = this.fetchPaymentsData();
+			}, 1000);
+		},
 	};
 </script>
 
 <style lang="scss">
+	header {
+		margin-bottom: 40px;
+	}
 	#app {
 		font-family: Avenir, Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
@@ -25,5 +79,23 @@
 		text-align: center;
 		color: #2c3e50;
 		margin-top: 60px;
+	}
+	.dataList {
+		margin-top: 40px;
+	}
+	main button {
+		width: 200px;
+		padding: 10px;
+		margin: 0 auto;
+		background-color: green;
+		border: 1px solid rgb(2, 51, 2);
+		cursor: pointer;
+		color: white;
+		position: relative;
+	}
+	main button::after {
+		content: "+";
+		position: absolute;
+		right: 10px;
 	}
 </style>
---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="input__container">
+		<input type="text" placeholder="Value" v-model="value" />
+		<input type="text" placeholder="Category" v-model="category" />
+		<input type="text" placeholder="Date" v-model="date" />
+		<button @click="addPayment">Add</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "AddPaymentForm",
+		data: () => ({
+			value: "",
+			category: "",
+			date: "",
+		}),
+		methods: {
+			addPayment() {
+				const { value, category, date, currenDate } = this;
+				const data = {
+					value,
+					category,
+					date: date != "" ? date : currenDate,
+				};
+				this.$emit("add-payment", data);
+			},
+		},
+		computed: {
+			currenDate() {
+				const currenDate = new Date();
+				const day = currenDate.getDate();
+				const month = currenDate.getMonth() + 1;
+				const year = currenDate.getFullYear();
+				return `${day}.${month}.${year}`;
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.input__container {
+		display: flex;
+		flex-direction: column;
+		width: 300px;
+		margin: 20px auto;
+	}
+	.input__container input {
+		padding: 10px;
+		border-radius: 10px;
+		outline: none;
+		margin-bottom: 5px;
+		border: 1px solid green;
+	}
+	.input__container button {
+		width: 100px;
+		padding: 5px;
+		margin: 0 auto;
+		background-color: green;
+		border: 1px solid rgb(2, 51, 2);
+		cursor: pointer;
+		color: white;
+		position: relative;
+	}
+	.input__container button::after {
+		content: "+";
+		position: absolute;
+		right: 10px;
+	}
+</style>
